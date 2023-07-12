1e13190cf1896b02a24f3f1e3cc1c0148096af51
MILAGROS-47 <131300024+MILAGROS-47@users.noreply.github.com>
2023-07-11 21:20:02 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    <script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+</head>
+
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+
+    <div class="container">
+        <form action="login.php" method="POST">
+            
+            <h1 class="h3 mb-3 fw-normal">Login</h1>
+
+            <div class="form-floating mb-1">
+                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com"
+                    required>
+                <label for="email">Email address</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password"
+                    required>
+                <label for="password">Password</label>
+            </div>
+
+            
+            <input class="btn btn-primary w-100 py-2" type="submit" value="Iniciar Sesión">
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
